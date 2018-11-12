6e3c997783e1c633d0cf8d6b255a10d404ef6e9a
Paolo Grasso <paolo.1995@ymail.com>
2018-11-12 10:46:26 +0100

    Modify formulas for mean and covariance
    - MLE mean and covariance matrix
    - Add comments

--- Lab 2/L2es1.m
+++ Lab 2/L2es1.m
@@ -1,16 +1,16 @@
 close all; clc; clear
 data = load('heightWeight.mat')
 
-cartaDaZucchero = [137; 207; 240]/255;
-ametista = [153; 102; 204]/255;
+cartaDaZucchero = [137; 207; 240]/255;  % Color for plots
+ametista = [153; 102; 204]/255;  % Color for plots
 
 male = data.heightWeightData(data.heightWeightData(:,1) == 1,2:end);
 female = data.heightWeightData(data.heightWeightData(:,1) == 2,2:end);
 
-figure(1)
+figure(1)  % Scatter plot (males and females)
 hold on
-scatter(male(:,1),male(:,2),'ob')
-scatter(female(:,1),female(:,2),'om')
+scatter(male(:,1),male(:,2),'^b')
+scatter(female(:,1),female(:,2),'vm')
 grid on
 grid minor
 xlabel('Height (cm)')
@@ -20,7 +20,7 @@ xlim([120 220])
 ylim([30 130])
 axis equal
 
-figure(2)
+figure(2)  % Histogram (males)
 hold on
 edges=120:5:220;
 h1 = histcounts(male(:,1),edges);
@@ -34,7 +34,7 @@ grid on
 grid minor
  legend('Males','Females','location','northeast')
  
-figure(3)
+figure(3)  % Histogram (females)
 hold on
 edges=30:5:130;
 h1 = histcounts(male(:,2),edges);
@@ -48,35 +48,43 @@ grid on
 grid minor
 legend('Male','Female','location','northeast')
 
-%figure(2)
-% hold on
-% histogram(male(:,1),'binwidth',1)
-% histogram(female(:,1),'binwidth',1)
-% grid on
-% grid minor
-% xlabel('Height (cm)')
-% ylabel('Weight (kg)')
-% legend('Male','Female','location','northwest')
-% 
-% figure(3)
-% hold on
-% histogram(male(:,2),'binwidth',1)
-% histogram(female(:,2),'binwidth',1)
-% grid on
-% grid minor
-% xlabel('Height (cm)')
-% ylabel('Weight (kg)')
-% legend('Male','Female','location','northwest')
+% MLE mean (males).
+mMales = 0;
+for i = 1:length(male)
+    mMales = mMales + male(i,:);
+end
+mMales = mMales/length(male)
 
-mMale = [mean(male)]
-mFemale = [mean(female)]
-cMale = [cov(male)]
-cFemale = [cov(female)]
+% MLE mean (females).
+mFemales = 0;
+for i = 1:length(female)
+    mFemales = mFemales + female(i,:);
+end
+mFemales = mFemales/length(female)
 
+% MLE covariance (males).
+firstTerm = zeros(2);  % 2x2 matrix of zeros
+for i = 1:length(male)
+    firstTerm = firstTerm + male(i,:)'*male(i,:); 
+end
+firstTerm = firstTerm/length(male);
+secondTerm = mMales.*mMales';
+sigmaMales = firstTerm - secondTerm
+
+% MLE covariance (females).
+firstTerm = zeros(2);  % 2x2 matrix of zeros
+for i = 1:length(female)
+    firstTerm = firstTerm + female(i,:)'*female(i,:); 
+end
+firstTerm = firstTerm/length(female);
+secondTerm = mFemales.*mFemales';
+sigmaFemales = firstTerm - secondTerm
+
+% Multivariate gaussian plots.
 figure(4)
 x1 = 120:220; x2 = 30:130;
 [X1,X2] = meshgrid(x1,x2);
-F = mvnpdf([X1(:) X2(:)],mMale,cMale);
+F = mvnpdf([X1(:) X2(:)],mMales,sigmaMales);
 F = reshape(F,length(x2),length(x1));
 surf(x1,x2,F);
 caxis([min(F(:))-.5*range(F(:)),max(F(:))]);
@@ -88,7 +96,7 @@ colormap parula; view(0,90); axis equal; colorbar;
 figure(5)
 x1 = 120:220; x2 = 30:130;
 [X1,X2] = meshgrid(x1,x2);
-F = mvnpdf([X1(:) X2(:)],mFemale,cFemale);
+F = mvnpdf([X1(:) X2(:)],mFemales,sigmaFemales);
 F = reshape(F,length(x2),length(x1));
 surf(x1,x2,F);
 caxis([min(F(:))-.5*range(F(:)),max(F(:))]);
