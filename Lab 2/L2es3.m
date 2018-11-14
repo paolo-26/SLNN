370cb4ee780e7a3159c4ee9da06353d0f871748e
Paolo Grasso <paolo.1995@ymail.com>
2018-11-14 09:54:35 +0100

    Change legend

--- Lab 2/L2es3.m
+++ Lab 2/L2es3.m
@@ -32,13 +32,16 @@ for j = 1:length(theta)
     I(j) = sum(theta(j,:).*pie(1,:).*log((theta(j,:)+eps)/(thetaJ+eps))+...
         (1-theta(j,:)).*pie(1,:).*log((1-theta(j,:)+eps)/(1-thetaJ+eps)));
 end
+
 I = I';  % Make I a column vector
 I(:,2) = 1:length(I);  % Add indexing to I
 I = sortrows(I,1,'descend');  % Sort: most important words
 
 o = 1;
+
 for K = 1:600  % Accuracy for each K
 features = I(1:K,2);  % Most important words
+
 for k = 1:length(data.xtrain)
     resTrain(k,:) = sum(log(theta(features(data.xtrain(k,features)==1),:))) +...
         sum(log(1-theta(features(data.xtrain(k,features)==0),:))) + log(pie(1));
@@ -51,7 +54,6 @@ end
 
 classesTrain = (resTrain(:,1) < resTrain(:,2)) + 1;  % Choose class
 classesTest = (resTest(:,1) < resTest(:,2)) + 1;  % Choose class
-
 acc(o,1) = sum(classesTrain == data.ytrain) / length(data.ytrain) * 100;
 acc(o,2) = sum(classesTest == data.ytest) / length(data.ytest) * 100;
 o = o + 1;
@@ -61,7 +63,7 @@ figure(1)
 hold on
 plot(acc(:,1)/100,'.-')
 plot(acc(:,2)/100,'.-')
-legend('Train', 'Test', 'location','best')
+legend('Training', 'Test', 'location','best')
 title('Naive Bayes classifier')
 grid on
 grid minor
