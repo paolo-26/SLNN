80d10a8c338792c27ee28811e151d13e271f5f07
Paolo Grasso <paolo.1995@ymail.com>
2018-11-21 11:48:54 +0100

    Fix pi formula
    - Fix pi formula
    - Change variable names

--- Lab 2/L2es4.m
+++ Lab 2/L2es4.m
@@ -1,6 +1,5 @@
 clear; close all; clc
 
-%% DATA
 data = load('heightWeight')
 SPLIT_M = 25
 SPLIT_F = 40
@@ -8,44 +7,52 @@ SPLIT_F = 40
 males = data.heightWeightData(data.heightWeightData(:,1) == 1,2:end);
 females = data.heightWeightData(data.heightWeightData(:,1) == 2,2:end);
 
-test_males = males(1:SPLIT_M,:);
-train_males = males(SPLIT_M+1:end,:);
+LM = length(males);
+LF = length(females);
 
-test_females = females(1:SPLIT_F,:);
-train_females = females(SPLIT_F+1:end,:);
+testMales = males(1:SPLIT_M,:);
+trainMales = males(SPLIT_M+1:end,:);
 
-for run = 1:3
-%% PART 1
+testFemales = females(1:SPLIT_F,:);
+trainFemales = females(SPLIT_F+1:end,:);
+
+for run = 1:3  % Run three times with different covariance matrices
 % MLE mean (males).
 mM = 0;
-for i = 1:length(train_males)
-    mM = mM + train_males(i,:);
+
+for i = 1:length(trainMales)
+    mM = mM + trainMales(i,:);
 end
-mM = mM/length(train_males)
+
+mM = mM/length(trainMales)
 
 % MLE mean (females).
 mF = 0;
-for i = 1:length(train_females)
-    mF = mF + train_females(i,:);
+
+for i = 1:length(trainFemales)
+    mF = mF + trainFemales(i,:);
 end
-mF = mF/length(train_females)
+
+mF = mF/length(trainFemales)
 
 % MLE covariance (males).
 firstTerm = zeros(2);  % 2x2 matrix of zeros
-for i = 1:length(train_males)
-    firstTerm = firstTerm + train_males(i,:)'*train_males(i,:); 
+
+for i = 1:length(trainMales)
+    firstTerm = firstTerm + trainMales(i,:)'*trainMales(i,:); 
 end
-firstTerm = firstTerm/length(train_males);
+
+firstTerm = firstTerm/length(trainMales);
 secondTerm = mM.*mM';
 sM = firstTerm - secondTerm
 
 if run == 2
-   sM = diag(diag(sM)) 
+   sM = diag(diag(sM))  % Set to zero off-diagonal elements.
 end
 
 if run == 3
     firstTerm = zeros(2);  % 2x2 matrix of zeros
-    shared = [train_males; train_females]
+    shared = [trainMales; trainFemales];
     for i = 1:length(shared)
         firstTerm = firstTerm + shared(i,:)'*shared(i,:); 
     end
@@ -56,56 +63,56 @@ end
 
 % MLE covariance (females).
 firstTerm = zeros(2);  % 2x2 matrix of zeros
-for i = 1:length(train_females)
-    firstTerm = firstTerm + train_females(i,:)'*train_females(i,:); 
+for i = 1:length(trainFemales)
+    firstTerm = firstTerm + trainFemales(i,:)'*trainFemales(i,:); 
 end
-firstTerm = firstTerm/length(train_females);
+firstTerm = firstTerm/length(trainFemales);
 secondTerm = mF.*mF';
 sF = firstTerm - secondTerm
 
 if run == 2
-   sF = diag(diag(sF)) 
+   sF = diag(diag(sF))  % Set to zero off-diagonal elements.
 end
 
 if run == 3
-   sf = sM 
+   sf = sM  % Covariance matrices are the same shared covarianca matrix.
 end
-L = length(data.heightWeightData);
-pie(1) = [(L-SPLIT_M)/((L-SPLIT_M)+(L-SPLIT_F))];
-pie(2) = [(L-SPLIT_F)/((L-SPLIT_M)+(L-SPLIT_F))];
+
+pie(1) = [(LM-SPLIT_M)/((LM-SPLIT_M)+(LF-SPLIT_F))];  %  Males
+pie(2) = [(LF-SPLIT_F)/((LM-SPLIT_M)+(LF-SPLIT_F))];  %  Females
 
 %% PART 1
 
-x = [test_males; test_females];
+x = [testMales; testFemales];
 for i = 1:length(x)
-    num1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
+    num = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
     den1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
     den2 = pie(2)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
-    posteriorM(i) = num1/(den1+den2);
+    posteriorM(i) = num/(den1+den2);  % Prob. of being male
 end
 
-x = [test_males; test_females];
+x = [testMales; testFemales];
 for i = 1:length(x)
-    num1 = pie(1)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
+    num = pie(2)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
     den1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
     den2 = pie(2)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
-    posteriorF(i) = num1/(den1+den2);
+    posteriorF(i) = num/(den1+den2);  % Prob. of being female
 end
 % 
-% x = test_females;
+% x = testFemales;
 % for i = 1:length(x)
-%     num1 = pie(1)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
+%     num = pie(1)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
 %     den1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
 %     den2 = pie(2)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
-%     posteriorFF(i) = num1/(den1+den2);
+%     posteriorFF(i) = num/(den1+den2);
 % end
 % 
-% x = test_females;
+% x = testFemales;
 % for i = 1:length(x)
-%     num1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
+%     num = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
 %     den1 = pie(1)*(norm(2*pi*sM)^(-1/2))*(exp(-1/2*(x(i,:)-mM)*inv(sM)*(x(i,:)-mM)'));
 %     den2 = pie(2)*(norm(2*pi*sF)^(-1/2))*(exp(-1/2*(x(i,:)-mF)*inv(sF)*(x(i,:)-mF)'));
-%     posteriorFM(i) = num1/(den1+den2);
+%     posteriorFM(i) = num/(den1+den2);
 %end
 
 classified(run) = (sum(posteriorM(1:SPLIT_M) > posteriorF(1:SPLIT_M))+sum(posteriorM(SPLIT_M+1:end) < posteriorF(SPLIT_M+1:end))) /(SPLIT_M+SPLIT_F)
@@ -115,16 +122,16 @@ end
 %% GRAPH
 
 figure(5)
-scatter(test_males(:,1),test_males(:,2), '.')
+scatter(testMales(:,1),testMales(:,2),100, '.')
 hold on
 grid minor
-scatter(test_females(:,1),test_females(:,2), '.')
-M = (mean(test_males));
+scatter(testFemales(:,1),testFemales(:,2),100, '.')
+M = (mean(testMales));
 %scatter(M(1),M(2),100, 'kx')
-F = (mean(test_females));
+F = (mean(testFemales));
 %scatter(F(1),F(2),100, 'kx')
 MF = mean([M; F]);
-scatter(MF(1),MF(2),100, 'mx')
+%scatter(MF(1),MF(2),100, 'mx')
 axis equal
 xlabel('Height')
 ylabel('Weight')
