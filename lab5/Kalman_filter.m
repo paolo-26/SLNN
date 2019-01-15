e4be6f4e67f90c6fa030913d57e29789253efc86
Paolo Grasso <paolo.1995@ymail.com>
2019-01-15 18:44:37 +0100

    Update file

---
+++ lab5/Kalman_filter.m
@@ -0,0 +1,109 @@
+clear all; close all; clc;
+% Simulation of object motion.
+
+rng(44)  % Seed for random numbers
+N = 100;  % Number of time instants
+DELTA = 1.5;
+A = [1 0 DELTA 0; 0 1 0 DELTA; 0 0 1 0; 0 0 0 1];
+
+sigmaQx = 2;
+sigmaQv = 0.5;
+
+epsilon = zeros(4,N);
+epsilon(1:2,:) = sigmaQx * randn(2,N);
+epsilon(3:4,:) = sigmaQv * randn(2,N);
+z = zeros(4,N);  % State vector (over time)
+z(:,1) = [0 0 DELTA DELTA].'; % Initial state: coordinates at time 0 are (0,0)
+for i = 2 : N
+    z(:,i) = A * z(:,i-1) + epsilon(:,i);
+end
+
+C = [1 0 0 0; 0 1 0 0];
+sigmaR = 20;
+delta = sigmaR * randn(2,N);
+y = zeros(2,N);
+y(:,1) = [0 0].';
+for i = 2 : N
+    y(:,i) = C * z(:,i) + delta(:,i);
+end
+
+
+
+%% Kalman Filter
+sigmaQx = 0.01;  % Variance of model
+sigmaQv = 0.01;  % Variance of model
+sigmaR = 300;  % Variance of observed data
+
+mut = zeros(4, N);
+mut(:,1) = [0,0,1.5,1.5]';
+%mut(:,1) = [78, 5, 45, 1]';
+Qt = diag([sigmaQx, sigmaQx, sigmaQv, sigmaQv]);
+Rt = diag([sigmaR; sigmaR]);
+%sigmat = eye(4);
+sigmat = 100*rand(4,4);
+pzt = zeros(4,N);
+pzt(:,1) = [0 0 DELTA DELTA].'; 
+
+for i = 2:N
+    mut_tp = A*mut(:,i-1);
+    sigmat_tp = A*sigmat*A.'+ Qt;
+    yhat(:,i) = C*mut_tp;
+    Kt = sigmat_tp*C'*inv(C*sigmat_tp*C.'+Rt);
+    mut(:,i) = mut_tp + Kt*(y(:,i)-yhat(:,i));
+    sigmat = (eye(4)-Kt*C)*sigmat_tp;
+    %pzt(:,i) = sigmat*randn(mut,1);
+end
+
+
+%% Plots
+% This figure plots object motion trajectory.
+figure(1)
+plot(z(1,:), z(2,:))
+grid minor; axis square
+xlabel('\it x'); ylabel('\it y')
+title('Object motion trajectory')
+
+figure(2)
+hold on
+grid on
+grid minor
+xlabel('\it t'); ylabel('\it x');
+title('Position vs time')
+plot(y(1,:))
+plot(yhat(1,:))
+plot(mut(1,:),'k:', 'linewidth',2)
+plot(z(1,:))
+legend('Observed','Estimated','Filtered','Real', 'location', 'best')
+
+figure(3)
+hold on
+grid on
+grid minor
+xlabel('\it t'); ylabel('\it x');
+title('Position vs time')
+plot(y(2,:))
+plot(yhat(2,:))
+plot(mut(2,:),'k:', 'linewidth',2)
+plot(z(2,:))
+legend('Observed','Estimated','Filtered','Real', 'location', 'best')
+
+% figure(4)
+% hold on
+% grid on
+% grid minor
+% xlabel('\it t'); ylabel('\it y');
+% title('Velocity_x vs time')
+% plot(mut(3,:),':', 'linewidth',2)
+% plot(z(3,:), 'linewidth',2)
+% legend('Filtered','Real', 'location', 'best')
+% 
+% figure(5)
+% hold on
+% grid on
+% grid minor
+% xlabel('\it t'); ylabel('\it x');
+% title('Velocity_y vs time')
+% plot(mut(4,:),':', 'linewidth',2)
+% plot(z(4,:), 'linewidth',2)
+% legend('Filtered','Real', 'location', 'best')
+
