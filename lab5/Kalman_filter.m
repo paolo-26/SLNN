0a529555bee9f3dc38ede7d4050076da67b275ab
Paolo Grasso <paolo.1995@ymail.com>
2019-01-17 10:16:17 +0100

    Update plots

--- lab5/Kalman_filter.m
+++ lab5/Kalman_filter.m
@@ -30,27 +30,27 @@ end
 
 
 %% Kalman Filter
-sigmaQx = 0.01;  % Variance of model
-sigmaQv = 0.01;  % Variance of model
-sigmaR = 300;  % Variance of observed data
+sigmaQx = 0.2;  % Variance of model
+sigmaQv = 0.2;  % Variance of model
+sigmaR = 20;  % Variance of observed data
 
 mut = zeros(4, N);
 mut(:,1) = [0,0,1.5,1.5]';
 %mut(:,1) = [78, 5, 45, 1]';
 Qt = diag([sigmaQx, sigmaQx, sigmaQv, sigmaQv]);
 Rt = diag([sigmaR; sigmaR]);
-%sigmat = eye(4);
-sigmat = 100*rand(4,4);
+sigmat = eye(4);
+%sigmat = 100*rand(4,4);
 pzt = zeros(4,N);
 pzt(:,1) = [0 0 DELTA DELTA].'; 
 
 for i = 2:N
     mut_tp = A*mut(:,i-1);
-    sigmat_tp = A*sigmat*A.'+ Qt;
-    yhat(:,i) = C*mut_tp;
-    Kt = sigmat_tp*C'*inv(C*sigmat_tp*C.'+Rt);
-    mut(:,i) = mut_tp + Kt*(y(:,i)-yhat(:,i));
-    sigmat = (eye(4)-Kt*C)*sigmat_tp;
+    sigmat_tp = A * sigmat * A.' + Qt;
+    yhat(:,i) = C * mut_tp;
+    Kt = sigmat_tp * C' * inv(C*sigmat_tp*C.' + Rt);
+    mut(:,i) = mut_tp + Kt * (y(:,i) - yhat(:,i));
+    sigmat = (eye(4) - Kt*C) * sigmat_tp;
     %pzt(:,i) = sigmat*randn(mut,1);
 end
 
@@ -68,10 +68,10 @@ hold on
 grid on
 grid minor
 xlabel('\it t'); ylabel('\it x');
-title('Position vs time')
+title('X position vs time')
 plot(y(1,:))
 plot(yhat(1,:))
-plot(mut(1,:),'k:', 'linewidth',2)
+plot(mut(1,:),'k:', 'linewidth',1.5)
 plot(z(1,:))
 legend('Observed','Estimated','Filtered','Real', 'location', 'best')
 
@@ -79,12 +79,12 @@ figure(3)
 hold on
 grid on
 grid minor
-xlabel('\it t'); ylabel('\it x');
-title('Position vs time')
-plot(y(2,:))
-plot(yhat(2,:))
-plot(mut(2,:),'k:', 'linewidth',2)
-plot(z(2,:))
+xlabel('\it t'); ylabel('\it y');
+title('Y position vs time')
+plot(y(2,:), 'color', [0, 0.4470, 0.7410])
+plot(yhat(2,:), 'color', [0.6350, 0.0780, 0.1840])
+plot(mut(2,:),'k:', 'linewidth',1.5)
+plot(z(2,:), 'color', [0.9290, 0.6940, 0.1250])
 legend('Observed','Estimated','Filtered','Real', 'location', 'best')
 
 % figure(4)
